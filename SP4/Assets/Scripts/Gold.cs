a393c9202eddf1ff9dbcd6674f0140d007ef3817
zoe <hyuikusyuan@gmail.com>
2015-02-17 16:55:43 +0800

    completed gold

---
+++ SP4/Assets/Scripts/Gold.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gold : MonoBehaviour 
+{
+	public int gold;
+	private float rtime;
+	bool collectgold;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gold = 0;
+		guiText.text = "Gold : " + gold;
+		rtime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Time.time - rtime > 1.0f) 
+		{
+			gold += 1;
+			rtime = Time.time;
+			guiText.text = "Gold : " + gold;
+		}
+	}
+}
---
+++ SP4/Assets/Scripts/Human.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour 
+{
+	Vector3 position;
+	Vector3 direction;
+	public int state;
+
+	public int damage;
+	float speed = 0.3f;
+	public int health;
+
+	// Use this for initialization
+	void Start () 
+	{
+//		damage = 5;
+//		health = 100;
+
+//		guiText.text = "Damage : " + damage;
+//		guiText.text = "Health : " + health;
+//		guiText.text = "Speed : " + speed;
+//		guiText.text = "Position : " + position;
+//		guiText.text = "Direction : " + direction;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch (state) 
+		{
+			case 1: //idle
+			{
+				//transform.Translate (speed * direction.x, speed * direction.y, 0);
+				transform.Translate (speed * direction.x, 0, 0);
+				if (transform.position.x > 50.0f || transform.position.x < 9.9f) 
+				{
+					direction = -direction;
+				}
+//				if (transform.position.y > 50.0f || transform.position.y < 9.9f)
+//				{
+//					direction = -direction;
+//				}
+			}
+			break;
+		}
+	}
+}
