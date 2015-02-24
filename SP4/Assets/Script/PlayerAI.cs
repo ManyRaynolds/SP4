dedf02c9f99deaa203b8c02725f5ee268a9ce74c
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-02-24 17:40:13 +0800

    Add little ai, very little

---
+++ SP4/Assets/Script/PlayerAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAI : MonoBehaviour {
+
+	public int player_health = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
