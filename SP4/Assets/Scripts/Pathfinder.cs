85187fcf57c450efd2179643f28c1712b276227a
Eikatsu <hayashieikata@hotmail.co.jp>
2015-02-24 14:39:16 +0800

    Semi-Static path finding done.

--- SP4/Assets/Scripts/Pathfinder.cs
+++ SP4/Assets/Scripts/Pathfinder.cs
@@ -1,66 +1,81 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Pathfinding : MonoBehaviour {
-	
-	public LayerMask unwalkableMask;
-	public Vector2 gridWorldSize;
-	public float nodeRadius;
-	Node[,] grid;
-	
-	float nodeDiameter;
-	int gridSizeX, gridSizeY;
-	
-	void Start() {
-		nodeDiameter = nodeRadius*2;
-		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
-		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
-		CreateGrid();
+public class Pathfinder : MonoBehaviour {
+	public Transform seeker, target;
+
+	GridNode grid;
+
+	void Awake() {
+		grid = GetComponent<GridNode>();
 	}
-	
-	void CreateGrid() {
-		grid = new Node[gridSizeX,gridSizeY];
-		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward*     gridWorldSize.y/2;
-		
-		for (int x = 0; x < gridSizeX; x ++) {
-			for (int y = 0; y < gridSizeY; y ++) {
-				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
-				grid[x,y] = new Node(walkable,worldPoint);
+
+	void Update() {
+		FindPath(seeker.position, target.position);
+	}
+
+	void FindPath(Vector3 startPos, Vector3 targetPos) {
+		Node startNode = grid.NodeFromWorldPoint (startPos);
+		Node targetNode = grid.NodeFromWorldPoint (targetPos);
+
+		List<Node> openSet = new List<Node>();
+		HashSet<Node> closedSet = new HashSet<Node> ();
+		openSet.Add(startNode);
+
+		while (openSet.Count > 0) {
+			//optimising for Heap Later on
+			Node currentNode = openSet[0];
+			for (int i = 1; i < openSet.Count; i++){
+				if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost) {
+					currentNode = openSet[i];
+				}
+			}
+			openSet.Remove (currentNode);
+			closedSet.Add(currentNode);
+
+			if (currentNode == targetNode) {
+				RetracePath(startNode, targetNode);
+				return;
+			}
+
+			foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+					continue;
+				}
+
+				//cost of current node getting to neighbour node
+				int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+				if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+					neighbour.gCost = newMovementCostToNeighbour;
+					neighbour.hCost = GetDistance (neighbour, targetNode);
+					neighbour.parent = currentNode;
+
+					if (!openSet.Contains(neighbour))
+						openSet.Add(neighbour);
+				}
 			}
 		}
 	}
-	
-	public Node NodeFromWorldPoint(Vector3 worldPosition) {
-		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
-		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
-		percentX = Mathf.Clamp01(percentX);
-		percentY = Mathf.Clamp01(percentY);
-		
-		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
-		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
-		return grid[x,y];
-	}
-	
-	void OnDrawGizmos() {
-		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
-		
-		if (grid != null) {
-			foreach (Node n in grid) {
-				Gizmos.color = (n.walkable)?Color.white:Color.red;
-				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
-			}
-		}
-	}
-}
 
-public class Node {
-	
-	public bool walkable;
-	public Vector3 worldPosition;
-	
-	public Node(bool _walkable, Vector3 _worldPos) {
-		walkable = _walkable;
-		worldPosition = _worldPos;
+	void RetracePath(Node startNode, Node endNode) {
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		grid.path = path;
+	}
+
+	int GetDistance(Node nodeA, Node nodeB) {
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		if (dstX > dstY)
+			return 14 * dstY + 10 * (dstX - dstY);
+		return 14 * dstX + 10 * (dstY - dstX);
 	}
 }
