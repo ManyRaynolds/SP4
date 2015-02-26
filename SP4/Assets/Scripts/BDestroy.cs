8febaa6e6d53f6d5263572eaa27fdd40b6a7d1a4
zoe <hyuikusyuan@gmail.com>
2015-02-26 18:27:22 +0800

    Units, Bullets. Factions added.

---
+++ SP4/Assets/Scripts/BDestroy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BDestroy : MonoBehaviour 
+{
+	public Units tank , human;
+	public Units Ibullet;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+	}
+
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		if (col.gameObject.tag == "JTank") 
+		{			
+			tank = col.gameObject.GetComponent<Units>();
+			if(tank != null)
+			{
+				tank.health -= 10.0f;
+				Destroy(gameObject);
+			}
+		}
+
+		else if (col.gameObject.tag == "JHuman") 
+		{			
+			human = col.gameObject.GetComponent<Units>();
+			if(human != null)
+			{
+				human.health -= 10.0f;
+				Destroy(gameObject);
+			}
+		}
+
+		else if (col.gameObject.tag == "JBullet") 
+		{
+		}
+	}
+}
