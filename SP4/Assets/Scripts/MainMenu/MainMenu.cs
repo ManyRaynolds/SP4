56d2a76de522223448b8882592754fff06503730
Gabriel Goh <gabbi9955@hotmail.com>
2015-02-26 15:34:52 +0800

    Added Menu Stuff

---
+++ SP4/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GUIStyle StartGame;
+	public GUIStyle Settings;
+	public GUIStyle Credits;
+	public GUIStyle ExitGame;
+
+	bool buttonpressed = false;
+
+	public static string url = "127.0.0.1";
+
+//	public static AudioSource bgm;
+//	public static float volume = 0.5f;
+
+
+	//AudioClip audioclips = AudioClip.Create("Burn", 44100, 1, 44100, false, true);
+	// Use this for initialization
+	void Start () 
+	{
+//		bgm = this.audio;
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+	
+	void OnGUI(){
+		//buttonpressed = true;
+		if(!buttonpressed)
+		{
+			if(GUI.Button(new Rect(((float)Screen.width/100*38), (float)Screen.height/100*36, (float)Screen.width/100*25, (float)Screen.height/100*10), "", StartGame))
+			{
+				buttonpressed = true;
+				Initiate.Fade("LoginMenu", Color.black,0.5f);
+				Debug.Log("Start Game");
+				//GUI.Button = null;
+			}
+
+			else if(GUI.Button(new Rect(((float)Screen.width/100*38), (float)Screen.height/100*47, (float)Screen.width/100*25, (float)Screen.height/100*10), "", Settings))
+			{
+				buttonpressed = true;
+				Initiate.Fade("Settings", Color.black,0.5f);
+				Debug.Log("Settings");
+			}
+
+			else if(GUI.Button(new Rect(((float)Screen.width/100*38), (float)Screen.height/100*58, (float)Screen.width/100*25, (float)Screen.height/100*10), "", Credits))
+			{
+				buttonpressed = true;
+				Initiate.Fade("Credits", Color.black,0.5f);
+				Debug.Log("Credits");
+
+			}
+
+			else if(GUI.Button(new Rect(((float)Screen.width/100*38), (float)Screen.height/100*69, (float)Screen.width/100*25, (float)Screen.height/100*10), "", ExitGame))
+			{
+				Application.Quit();
+				Debug.Log("Exit Game");
+			}
+
+	//		if (GUI.Button(new Rect(((float)Screen.width*0.75), ((float)Screen.height*0.75), 100, 50), "Start Game"))
+	//			Debug.Log("Clicked the button with an image");
+		}
+//		DontDestroyOnLoad (bgm);
+		//DontDestroyOnLoad (audio);
+
+	}
+}
---
+++ SP4/Assets/Scripts/MainMenu/Settings.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+
+		public static float BGMSliderValue = 0.5f;
+		public static float BGMoldSliderValue = 0.5f;
+
+		public static float SFXSliderValue = 0.5f;
+		public static float SFXoldSliderValue = 0.5f;
+
+	public GUIStyle BGMMute;
+	public GUIStyle BGMMaxVolume;
+
+	public GUIStyle SFXMute;
+	public GUIStyle SFXMaxVolume;
+
+	public bool Sbuttonpress = false;
+
+	//public AudioSource bgm;
+	
+	// Use this for initialization
+	void Start () 
+	{
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Sound.bgm.volume = BGMSliderValue;
+	}
+
+	void OnGUI()
+	{
+		//hSliderValue = GUILayout.HorizontalSlider(hSliderValue, 0.0F, 10.0F);
+		//GUILayout.Label("Volume: " + hSliderValue);
+		//GUILayout.Height (10f);
+		if(!Sbuttonpress)
+		{
+			if(GUI.Button(new Rect(Screen.width/100*2, (float)Screen.height/100*1, Screen.width/100*15, Screen.height/100*10), "Return"))
+			{
+				Sbuttonpress = true;
+				//Initiate.Fade("Main Menu", Color.black,0.5f);
+				Initiate.Fade("Main Menu2", Color.black,0.5f);
+			}
+
+			//BGMSliderValue = GUI.HorizontalSlider(new Rect(300, 250, 200,100), BGMSliderValue, 0, 1);
+			BGMSliderValue = GUI.HorizontalSlider(new Rect(((float)Screen.width/100*40), ((float)Screen.height/100*39), ((float)Screen.width/100*23), 100), BGMSliderValue, 0, 1);
+			//SFXSliderValue = GUI.HorizontalSlider(new Rect(300, 350, 200,100), SFXSliderValue, 0, 1);
+			SFXSliderValue = GUI.HorizontalSlider(new Rect((float)Screen.width/100*40, (float)Screen.height/100*55, ((float)Screen.width/100*23),100), SFXSliderValue, 0, 1);
+			//audio.volume = SliderValue;
+			
+			GUI.Label(new Rect(370,200,100,50),"Volume: " + BGMSliderValue);
+
+			if(GUI.Button(new Rect(((float)Screen.width/100*33), (float)Screen.height/100*37, (float)Screen.width/100*4, Screen.height/100*7), "", BGMMute))
+			{
+				if(BGMSliderValue != 0)
+				{
+					BGMoldSliderValue = BGMSliderValue;
+					BGMSliderValue = 0;
+				}
+				//buttonpressed = true;
+				//Initiate.Fade("Credits", Color.green,0.5f);
+				Debug.Log("Mute");
+				//GUI.Button = null;
+
+			}
+			
+			if(GUI.Button(new Rect(((float)Screen.width/100*66), (float)Screen.height/100*36, (float)Screen.width/100*7, Screen.height/100*9), "", BGMMaxVolume))
+			{
+				if(BGMSliderValue == 0)
+				{
+					BGMSliderValue = BGMoldSliderValue;
+				}
+				Debug.Log("Volume: " + BGMSliderValue);
+			}
+
+
+			if(GUI.Button(new Rect(((float)Screen.width/100*33), (float)Screen.height/100*53, (float)Screen.width/100*4, Screen.height/100*7), "", SFXMute))
+			{
+				if(SFXSliderValue != 0)
+				{
+					SFXoldSliderValue = SFXSliderValue;
+					SFXSliderValue = 0;
+				}
+				//buttonpressed = true;
+				//Initiate.Fade("Credits", Color.green,0.5f);
+				Debug.Log("Mute");
+				//GUI.Button = null;
+			}
+			
+			if(GUI.Button(new Rect(((float)Screen.width/100*66), (float)Screen.height/100*52, (float)Screen.width/100*7, Screen.height/100*9), "", SFXMaxVolume))
+			{
+				if(SFXSliderValue == 0)
+				{
+					SFXSliderValue = SFXoldSliderValue;
+				}
+				Debug.Log("Volume: " + SFXSliderValue);
+			}
+
+
+
+			Debug.Log("BGM Volume: " + BGMSliderValue + "          SFX Volume: " + SFXSliderValue );
+		}
+		//}
+	}
+	public float getVolume()
+	{
+		return BGMSliderValue;
+	}
+}
+
+
