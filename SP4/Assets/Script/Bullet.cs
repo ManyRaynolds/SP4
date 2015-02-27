58ec5b078b70102e643cba2a25edac89eefe33da
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-02-27 11:51:38 +0800

    Add units interaction functions, unit attacking each other

---
+++ SP4/Assets/Script/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	
+	public float damage;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision col){
+		print (col.gameObject);
+		if (col.gameObject.tag == "bullet") {
+			return;		
+		}
+		col.gameObject.GetComponent<Units> ().health -= damage;
+		Destroy (this.gameObject);
+	}
+	
+}
