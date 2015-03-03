baeaf180439bbd4da4935308283c379926894afe
RaynoldTanYX <133121W@mymail.nyp.edu.sg>
2015-03-03 14:43:20 +0800

    Added:  - Gold  - Resource building adds gold over time  - Buildings cost gold to build

--- SP4/Assets/Scripts/Networking.cs
+++ SP4/Assets/Scripts/Networking.cs
@@ -35,6 +35,11 @@ public class Networking : MonoBehaviour {
 	}
 	public List<PlayerInformation> playerInfoList = new List<PlayerInformation>();
 
+	public int gold = 0;
+	public float goldTimer = 0;
+	public float goldTime = 0;
+	public int goldAmount = 0;
+
 	// Use this for initialization
 	void Start () {
 		Application.runInBackground = true;
@@ -50,6 +55,13 @@ public class Networking : MonoBehaviour {
 		if (Network.peerType == NetworkPeerType.Disconnected) {
 			chatLog.Clear();
 			currentMessage = "";
+		}
+		else{
+			goldTimer += Time.deltaTime;
+			if (goldTimer >= goldTime){
+				goldTimer -= goldTime;
+				gold += goldAmount;
+			}
 		}
 	}
 
@@ -142,17 +154,26 @@ public class Networking : MonoBehaviour {
 			}
 			
 			if (GUI.Button (new Rect(200.0f, 0.0f, 80, 25), "", unitstyle)){
-				AudioClip units = AudioClip.Create ("SFX/Units", 44100, 1, 44100, false, true);
-				sfx = this.audio;
-				Network.Instantiate(UnitBuildingPrefabs[0], Vector3.zero, Quaternion.identity, 0);
+				if (gold >= UnitBuildingPrefabs[0].GetComponent<Building>().cost){
+					AudioClip units = AudioClip.Create ("SFX/Units", 44100, 1, 44100, false, true);
+					sfx = this.audio;
+					gold -= UnitBuildingPrefabs[0].GetComponent<Building>().cost;
+					Network.Instantiate(UnitBuildingPrefabs[0], Vector3.zero, Quaternion.identity, 0);
+				}
 
 				//audio.Play ();
 			}
 			if (GUI.Button (new Rect(300.0f, 0.0f, 80, 25), "", resourcestyle)){
+				if (gold >= UnitBuildingPrefabs[1].GetComponent<Building>().cost){
+					gold -= UnitBuildingPrefabs[1].GetComponent<Building>().cost;
 				Network.Instantiate(UnitBuildingPrefabs[1], Vector3.zero, Quaternion.identity, 0);
+				}
 			}
-			if (GUI.Button (new Rect(400.0f, 0.0f, 100, 25), "Base")){
+				if (GUI.Button (new Rect(400.0f, 0.0f, 100, 25), "Base")){
+				if (gold >= UnitBuildingPrefabs[2].GetComponent<Building>().cost){
+					gold -= UnitBuildingPrefabs[2].GetComponent<Building>().cost;
 				Network.Instantiate(UnitBuildingPrefabs[2], Vector3.zero, Quaternion.identity, 0);
+					}
 			}
 //			if (GUI.Button (new Rect(200.0f, 0.0f, 125, 25), "Units")){
 //				Network.Instantiate(UnitBuildingPrefabs[0], Vector3.zero, Quaternion.identity, 0);
--- SP4/Assets/Scripts/ResourceBuilding.cs
+++ SP4/Assets/Scripts/ResourceBuilding.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class ResourceBuilding : Building {
 
+	public float resourceTime = 0;
+	public float resourceTimer = 0;
+
+	public int resourceAmount = 0;
+
+	public GameObject networkController;
+
 	// Use this for initialization
 	void Start () {
-	
+		networkController = GameObject.Find ("Network");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		UpdateBuilding ();
 
+		resourceTimer += Time.deltaTime;
+		if (resourceTimer >= resourceTime) {
+			resourceTimer -= resourceTime;
+			networkController.GetComponent<Networking>().gold += resourceAmount;
+		}
 	}
 }
