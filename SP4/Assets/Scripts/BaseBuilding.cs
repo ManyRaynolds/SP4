b81ec351b52f7b0150087e79b10739099fb1900c
RaynoldTanYX <133121W@mymail.nyp.edu.sg>
2015-03-04 19:57:44 +0800

    win lose condition done GABRIEL!!!! I NEED WIN AND LOSE SCREEN!!!

--- SP4/Assets/Scripts/BaseBuilding.cs
+++ SP4/Assets/Scripts/BaseBuilding.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class BaseBuilding : Building {
 
-	bool sentWinLose = false;
 
 	// Use this for initialization
 	void Start () {
@@ -12,26 +11,6 @@ public class BaseBuilding : Building {
 	
 	// Update is called once per frame
 	void Update () {
-
 		UpdateBuilding ();
-
-		if (destroyed){	
-			if (networkView.isMine){
-				if (!sentWinLose) {
-					//update lose
-
-					//send win to others
-					networkView.RPC ("SendWinLose", RPCMode.Others);
-				}	
-			}
-		}
-		else{
-			
-		}
 	}
-
-	[RPC]
-	void SendWinLose(){
-	}
-
 }
